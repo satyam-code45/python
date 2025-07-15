ff31657a34f9566af3afedf14f5af45abaad788f
satyam-code45 <satyam45.dev@gmail.com>
2025-07-15 21:52:16 +0530

    chapter3

---
+++ chapter3/string.py
@@ -0,0 +1,9 @@
+name = "satyam"
+
+nameShort = name[0:3]
+
+print(nameShort)
+
+character1 = name[0]
+
+print(character1)
---
+++ chapter3/stringfun.py
@@ -0,0 +1,8 @@
+name = "satyam"
+
+print(len(name))
+
+print(name.endswith("m"))
+print(name.startswith("s"))
+
+print(name.capitalize())
